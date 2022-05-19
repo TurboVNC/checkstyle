6338796ba5ae9bb83cf22fdea912fadc30118841
DRC <information@virtualgl.org>
2022-05-19 11:35:53 -0500

    Java: Ignore org.mindrot.jbcrypt.BCrypt

--- checkstyle.java
+++ checkstyle.java
@@ -8,6 +8,6 @@ else
 	FILES='*.java'
 fi
 
-git ls-files -- "$FILES" ':!java/com/jcraft' ':!java/org/spf4j' | while read file; do
+git ls-files -- "$FILES" ':!java/com/jcraft' ':!java/org/spf4j' ':!java/org/mindrot' | while read file; do
 	java -jar ~/.checkstyle/checkstyle-all.jar -c $SCRIPTDIR/sun_checks.xml $file >$SCRIPTDIR/checkstyle.report/`basename $file`.txt
 done
